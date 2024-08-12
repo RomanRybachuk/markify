74a15a26159cd44899e00d56b733c501e83ed812
RomanRybachuk <roman.rybachuk.work@gmail.com>
2024-08-12 13:23:52 +0300

    added sections

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
 "use strict";
 
 import "./scss/style.scss";
+
+const loader = document.querySelector(".loader-window");
+const links = document.querySelectorAll("a");
+
+window.addEventListener("load", () => {
+  loader.classList.add("hidden");
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = this.getAttribute("href");
+    loader.classList.remove("hidden");
+
+    setTimeout(() => {
+      window.location.href = href;
+    }, 200);
+  });
+});
